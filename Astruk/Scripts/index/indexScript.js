8dd217af7985fbd3bd8fe3f9459d63d0619627b6
bazyli <bazyli.reps@tutanota.com>
2019-01-25 15:28:11 +0100

    dodawanie/odejmowanie punktow kluczowych

--- Astruk/Scripts/index/indexScript.js
+++ Astruk/Scripts/index/indexScript.js
@@ -13,6 +13,19 @@ function sendAllData(allData) {
     const jsonDiv = document.getElementById("hiddenData");
     jsonDiv.insertAdjacentHTML("beforeend", jsonData);
 
+    const keyObjects = document.getElementById("keyObjectsList");
+    keyObjects.innerHTML = createKeyObjectsList(allData.KeyObjects);
+    let i = 1;
+    for (var vertex in allData.Vertices) {
+        vertex.Id = i++;
+    }
+
+    const verticesList = document.getElementById("verticesList");
+    verticesList.innerHTML = createVerticesList(allData.Vertices);
+
+    const dataHtml = document.getElementById("hiddenData");
+    dataHtml.innerHTML = JSON.stringify(allData);
+
     $("#map").load(url, allData);
 }
 
@@ -24,13 +37,14 @@ function addVertex() {
     const allData = JSON.parse(hiddenData.textContent);
     clearData();
 
-    const newId = allData.Vertices.length + 1;
+    const newId = allData.Vertices.length;
 
-    const newVertex = new Vertex(newId, xValue, yValue);
+    const newVertex = new Vertex(xValue, yValue);
+    newVertex.Id = newId;
     allData.Vertices.push(newVertex);
 
-    console.log(allData);
 
+    createVerticesList(allData.Vertices);
     sendAllData(allData);
     clearForms();
 }
@@ -50,8 +64,53 @@ function addKeyPoint() {
 
     allData.KeyObjects.push(newKeyPoint);
 
+    createKeyObjectsList(allData.Objects);
     sendAllData(allData);
     clearForms();
+}
+
+function deleteKeyObject(id) {
+    const allData = JSON.parse(document.getElementById('hiddenData').textContent);
+    const newKeyObjects = allData.KeyObjects.filter(function (keyObject) {
+        return keyObject.Id != id;
+    });
+
+    allData.KeyObjects = newKeyObjects;
+    sendAllData(allData);
+
+
+
+}
+
+function deleteVertex(id) {
+    const allData = JSON.parse(document.getElementById('hiddenData').textContent);
+    const newVertices = allData.Vertices.filter(function (vertex) {
+        return vertex.Id != id;
+    });
+    allData.Vertices = newVertices;
+    sendAllData(allData);
+}
+
+function createKeyObjectsList(objects) {
+    let i = 0;
+    let htmlString = "<ul style='overflow-y:scroll; height:300px'>";
+    for (; i < objects.length; i++) {
+        htmlString += `<div id="keyObject${objects[i].Id}">${objects[i].Name}: Id:${objects[i].Id}, x: ${objects[i].X}, y: ${objects[i].Y}`;
+        htmlString += ` <button id="deleteKeyObjectButton${objects[i].Id}" class="keyObjectsDeleteButton" onclick="deleteKeyObject(${objects[i].Id})" style="margin: auto"">usuń</button></div>`
+    }
+    htmlString += "</ul>";
+    return htmlString;
+}
+
+function createVerticesList(vertices) {
+    let i = 0;
+    let htmlString = "<ul style='overflow-y:scroll; height:300px'>";
+    for (; i < vertices.length; i++) {
+        htmlString += `<div id="vertex${i}">x: ${vertices[i].X}, y: ${vertices[i].Y}`;
+        htmlString += ` <button id="deleteVertexButton" class="verticesDeleteButton" onclick="deleteVertex(${vertices[i].Id})"">usuń</button></div>`
+    }
+    htmlString += "</ul>";
+    return htmlString;
 }
 
 function readFile(evt) {
