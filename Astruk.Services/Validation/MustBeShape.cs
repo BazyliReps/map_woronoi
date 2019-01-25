fe676ae065e0de5ee5ffb753a1ccbb665a0c5995
Sebastian Stasiak <s.stasiak@outlook.com>
2019-01-25 14:34:38 +0100

    Missing files

---
+++ Astruk.Services/Validation/MustBeShape.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Astruk.Common.Interfaces;
+using Astruk.Common.Models;
+
+namespace Astruk.Services.Validation
+{
+	internal class MustBeShape : IValidationRule<IList<Vertex>>
+	{
+		public string ErrorMessage => "Musi być więcej niż 3 wierzchołki";
+		public string ErrorKey => "vertices";
+		public bool IsCritical => false;
+
+		public bool IsValid(IList<Vertex> obj)
+		{
+			return obj.Count >= 3;
+		}
+	}
+}
---
+++ Astruk.Services/Validation/TypesExist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Astruk.Common.Interfaces;
+using Astruk.Common.Models;
+
+namespace Astruk.Services.Validation
+{
+	internal class TypesExist : IValidationRuleWithParameters<IEnumerable<MapObject>>
+	{
+		public TypesExist()
+		{
+			ParameterList = new List<object>();
+			ParameterTypes = new List<Type> {typeof(IEnumerable<MapObjectType>)};
+		}
+
+		private string NonExistingTypeName { get; set; }
+
+		public string ErrorMessage => $"Type {NonExistingTypeName} was not declared";
+		public string ErrorKey => "mapObjects";
+		public bool IsCritical => false;
+
+		public bool IsValid(IEnumerable<MapObject> obj)
+		{
+			var types = (IEnumerable<MapObjectType>) ParameterList[0];
+			foreach (var mapObject in obj)
+			{
+				if (types.Any(x => x.GetHashCode() == mapObject.Type.GetHashCode())) continue;
+				NonExistingTypeName = mapObject.Type.Name;
+				return false;
+			}
+
+			return true;
+		}
+
+		public List<object> ParameterList { get; }
+		public List<Type> ParameterTypes { get; }
+	}
+}
